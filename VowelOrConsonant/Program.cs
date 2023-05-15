987843c5e852dd9fe3425f23df0d1f115af63f8f
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-16 00:17:36 +0530

    [Hareesh]

---
+++ VowelOrConsonant/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace VowelOrConsonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an alphabet: ");
+            char alphabet = Convert.ToChar(Console.ReadLine().ToLower());
+
+            if (IsVowel(alphabet))
+            {
+                Console.WriteLine("The alphabet is a vowel.");
+            }
+            else
+            {
+                Console.WriteLine("The alphabet is a consonant.");
+            }
+        }
+
+        static bool IsVowel(char alphabet)
+        {
+            return alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u';
+        }
+    }
+}
