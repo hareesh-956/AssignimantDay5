87b0a84f09a603c59118481e0d38da61be933690
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-16 00:30:12 +0530

    [Hareesh]

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LargestAmongThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the third number: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int largest = FindLargest(num1, num2, num3);
+
+            Console.WriteLine($"The largest number is: {largest}");
+        }
+
+         static int FindLargest(int num1, int num2, int num3)
+        {
+            int largest = num1;
+
+            if (num2 > largest)
+            {
+                largest = num2;
+            }
+
+            if (num3 > largest)
+            {
+                largest = num3;
+            }
+
+            return largest;
+        }
+    }
+}
