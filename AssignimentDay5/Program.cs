fbce40df53c82b81d7e9ae5655c45f804856edc1
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-15 16:20:31 +0530

    [Hareesh]

---
+++ AssignimentDay5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AssignimentDay5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a year");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if (x > 2000 || x < 1000)
+            {
+                Console.WriteLine(" please enter four digit year ");
+            }
+            else if ((x % 4 == 0 && x % 100 != 0) || x % 400 == 0)
+            {
+                Console.WriteLine("{0} is a leap year.", x);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap year.", x);
+
+            }
+        }
+    }
+}
