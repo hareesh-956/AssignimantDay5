f52acf126cfebcd3656e351a2ce7439220f76517
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-15 23:53:19 +0530

    [Hareesh]

---
+++ SwapTwoNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SwapTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Before swapping: num1 = {num1}, num2 = {num2}");
+
+            // Swapping the numbers
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine($"After swapping: num1 = {num1}, num2 = {num2}");
+        }
+    }
+}
